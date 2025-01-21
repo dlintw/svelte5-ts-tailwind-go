4c5d1ba87b75d6eb1e9b7a535f219cf65b88d035
Your Name <YourEmail@example.com>
2025-01-21 08:10:46 +0800

    Separate Title to a Title.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  import Title from './lib/Title.svelte';
   import FriendAdder from "./lib/FriendAdder.svelte";
   import FriendList from "./lib/FriendList.svelte";
   import { loadSettings} from "./lib/db.svelte";
@@ -12,18 +11,7 @@
 </script>
 
 <main class="min-h-screen">
-  <div class="flex items-center gap-4">
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="h-24 p-6 will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#ff3e00aa]" alt="Svelte Logo" />
-    </a>
-    <h1 class="text-3xl  md:text-4xl">Svelte + TypeScript +  TailwindCss + Go</h1>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="h-[6em] p-[1.5em] will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite Logo" />
-    </a>
-  </div>
-  <p class="text-gray-300 bg-black">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Title />
   <FriendAdder />
   <br/>
   <FriendList />
