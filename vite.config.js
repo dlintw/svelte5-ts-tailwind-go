45726ec3ddbb41db2be3b04c607d2b5158e97a0b
Daniel YC Lin <dlin.tw@gmail.com>
2025-01-23 07:11:21 +0800

    put in subdirectory for preparing to publish on gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "svelte5-ts-tailwind-go",
   plugins: [svelte()],
 });
